358e4a2a1500bb33959cc58768cfb3798dbdd44a
abintt <77942722+abintt@users.noreply.github.com>
2021-01-25 11:12:53 -0400

    Add files via upload

---
+++ 1_basicProgram.py
@@ -0,0 +1,20 @@
+#This is the first part of the training course of Python
+
+#Instructor: Ricardo Bautista Quintero
+#Date: 24 January 2021 
+# NSCC
+#Ivany Campus
+#w0440636, w0441572,w0413470
+
+def program_1():
+    a=2
+    f=2e4
+    
+    print(a)
+    print("is an integer")
+    print(f)
+    print("is a floating point number")
+    
+if __name__== '__main__':
+        
+    program_1()
---
+++ readme.md
@@ -0,0 +1,1 @@
+In current directory the python program called Lab2.py is a domonstration of the use of how to print the message "Hello World" as well as basic numerical formats
